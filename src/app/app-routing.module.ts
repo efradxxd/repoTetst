8e9f90286e02c28eebbca8d2bcf6e96ede29cd5f
Fakturor <fakturor.admn@gmail.com>
2020-02-17 22:55:45 -0600

    insert image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LandingComponent } from './landing/landing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //redirect to home
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  },
+
+  {
+    path:'home',
+    component: LandingComponent
+  },
+
+  //404 error
+  {
+    path:'**',
+    component: NotFoundComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
